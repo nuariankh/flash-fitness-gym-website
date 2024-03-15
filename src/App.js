99ade5f4274ca9942f3b2a4ecedf99c1af687cc9
Nuari Williams <nuariwilliams@gmail.com>
2024-03-14 23:04:15 -0400

    Add HomePage sections and Footer js & styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-
-
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './app/pages/HomePage';
 import './App.css';
 
 function App() {
   return (
-    
-    
-    
-    
-    
-    
-    
+    <div className='App'>
+      <Header />
+      <HomePage />
+      <Footer />
+      {/* <Routes>
+        <Route path="/Home" element={} />
+        <Route path="/WorkoutBuilder" element={} />
+        <Route path="/Classes" element={} />
+        <Route path="/Trainers" element={} />
+        <Route path="/About" element={} />
+      </Routes> */}
+      <main>
+
+      </main>
+    </div>
     
     // <div className='App'>
     //   <Header />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,52 @@
 import { useState } from "react"
 import {
     Navbar,
-    NavbarBrand
+    NavbarBrand,
+    Container,
+    Nav,
+    NavItem,
+    NavbarToggler,
+    Collapse
 } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+import FlashFitnessLogo from '../app/assets/img/logo/flash-fitness-logo.png';
+
+/* 
+LIGHT-BLUE: #7CD0FA
+YELLOW: #FDC933
+DARK-BLUE: #002339
+*/
+
+const Header = () => {
+
+    const [menuOpen, setMenuOpen] = useState(false);  
+
+    return (
+        <Navbar dark sticky='top' expand='md' className="mb-0 m-0" style={{width: '100vw'}}>
+            <NavbarBrand className="header-container">
+                <img className="d-inline-block align-top header-logo" src={FlashFitnessLogo} alt="Flash Fitness Logo" style={{width: "100px", height: "100px"}} />
+                <h1 class="hero-header-text" style={{color: "#7CD0FA", display: "inline-block"}}>Flash Fitness</h1>
+            </NavbarBrand>
+            
+            <Nav className="ms-auto" navbar>
+                <NavItem>
+                    <NavLink className="nav-link">Home</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink className="nav-link">Workout Builder</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink className="nav-link">Classes</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink className="nav-link">Trainers</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink className="nav-link">About</NavLink>
+                </NavItem>
+            </Nav>
+        </Navbar>
+    )
+}
+
+export default Header;
