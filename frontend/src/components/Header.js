42b336c92abd7ff0e799546abdbc0f7505e9019e
Nuari Williams <nuariwilliams@gmail.com>
2024-07-19 15:16:35 -0400

    Frontend: Responsive CSS on Header and HomePage

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,3 @@
-import { useState } from "react"
 import {
     Navbar,
     NavbarBrand,
@@ -8,6 +7,7 @@ import {
     NavbarToggler,
     Collapse
 } from 'reactstrap';
+import './HeaderStyles.css'
 import { NavLink, Link } from 'react-router-dom';
 import FlashFitnessLogo from '../app/assets/img/logo/flash-fitness-logo.png';
 
@@ -19,14 +19,15 @@ DARK-BLUE: #002339
 
 const Header = () => {
 
-    const [menuOpen, setMenuOpen] = useState(false);  
-
     return (
         <Navbar dark sticky='top' expand='md' className="mb-0 m-0" style={{width: '100vw'}}>
             <NavbarBrand className="header-container">
-                <Link to='/'>
-                    <img className="d-inline-block align-top header-logo" src={FlashFitnessLogo} alt="Flash Fitness Logo" style={{width: "100px", height: "100px"}} />
-                    <h1 class="hero-header-text" style={{color: "#7CD0FA", display: "inline-block"}}>Flash Fitness</h1>
+                <Link to='/' style={{ textDecoration: 'none' }}>
+                <div style={{ display: 'flex' }}>
+                    <img className="align-top header-logo" src={FlashFitnessLogo} alt="Flash Fitness Logo" style={{width: "100px", height: "100px"}} />
+                    <h1 class="hero-header-text" style={{color: "#7CD0FA", fontSize: '2em', marginRight: '0' }}>Flash Fitness</h1>
+                </div>
+                    
                 </Link>
             </NavbarBrand>
             
