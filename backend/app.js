78d99d2e419f68b5f4cfef7f4c4681be32a1aee1
Nuari Williams <nuariwilliams@gmail.com>
2024-10-28 10:54:01 -0400

    Backend: Add CcorsOptions to app.use

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 
 const port = process.env.PORT || 8000;
 
-app.use(cors());
+app.use(cors(corsOptions));
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log('MongoDB Connected'))
