5cefb133ceb524d06c79f95b5cff1e0099eba109
Nuari Williams <nuariwilliams@gmail.com>
2024-11-13 15:50:14 -0500

    Frontend: Change Loading message

--- frontend/src/components/Loading.js
+++ frontend/src/components/Loading.js
@@ -8,6 +8,8 @@ const Loading = () => {
     return (
         <div>
             <img src={loadingIcon} style={{ width: '50px', height: '50px'}} />
+            <p>May take 30 seconds to 1 minute for our API to spin up</p>
+            <p>Thank you for your patience :)</p>
         </div>
     );
 };
